dbdf6af5d092312aa7baa0e461d312e80dd7cdad
Jørgen Christian Heinsen <c.heinsen@gmail.com>
2017-05-13 09:40:39 +0200

    Added initial project files.

---
+++ src/main/java/com/heinsen/robustruleclient/RobustRuleClientApplication.java
@@ -0,0 +1,12 @@
+package com.heinsen.robustruleclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobustRuleClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobustRuleClientApplication.class, args);
+	}
+}
